9222a17937c51f1bb469b45066828d7c83d615d2
Sameer <sameer@Sameer.(none)>
2014-10-03 14:49:53 -0400

    Working chat server with multiple clients. Works with firefox. Issues with chrome

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on('connection',function(client){
 	console.log('Client connected from address ' , client.handshake.address);	
-	client.emit('messages',{hello:'world'});
+	client.emit('messages',{hello:'Message from server'});
+	client.on('message',function(data){
+		client.broadcast.emit("messageFromClient",data);
+		console.log(data);
+	});
 });
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'jade');
-// app.set("view options", { layout: false });
 app.use(express.static(__dirname + '/public'));
-
-// app.get('/', function(req, res){
-//   res.render('index.html');
-// });
 
 server.listen(8080,function(){
 	console.log("Listening on 8080");
